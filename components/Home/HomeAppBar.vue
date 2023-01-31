ed7fbbabedcf542ed4a77558b7be6a04c04bbb28
kyainuma <yasu6491@icloud.com>
2023-02-01 08:25:29 +0900

    Add before-login-layout

--- components/Home/HomeAppBar.vue
+++ components/Home/HomeAppBar.vue
@@ -9,10 +9,9 @@
     <app-logo
       @click.native="$vuetify.goTo('#scroll-top')"
     />
-    <v-toolbar-title
+    <app-title
       class="hidden-mobile-and-down"
-    >
-    </v-toolbar-title>
+    />
     <v-spacer />
 
     <v-toolbar-items class="ml-2 hidden-ipad-and-down">
@@ -72,9 +71,8 @@ export default {
       default: 0
     }
   },
-  data ({ $config: { appName }, $store }) {
+  data ({ $store }) {
     return {
-      appName,
       scrollY: 0,
       homeAppBarHeight: $store.state.styles.homeAppBarHeight
     }
