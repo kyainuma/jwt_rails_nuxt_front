c3f048d2d1fc3297d87fb89495cfe9717f819e5f
kyainuma <yasu6491@icloud.com>
2023-02-01 09:52:21 +0900

    Finished before login layout

--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,60 @@
 <template>
-  <div>
-    login.vue
-  </div>
+  <user-form-card>
+    <template
+      #user-form-card-content
+    >
+      <v-form
+        ref="v-form"
+        v-model="isValid"
+      >
+        <user-form-email
+          :email.sync="params.user.email"
+        />
+        <user-form-password
+          :password.sync="params.user.password"
+        />
+        <v-card-actions>
+          <nuxt-link
+            to="#"
+            class="body-2 text-decoration-none"
+          >
+            パスワードを忘れた場合
+          </nuxt-link>
+        </v-card-actions>
+        <v-cart-text
+          class="px-0"
+        >
+          <v-btn
+            :disabled="!isValid || loading"
+            :loading="loading"
+            block
+            color="appblue"
+            class="white--text"
+            @click="login"
+          >
+            ログインする
+          </v-btn>
+        </v-cart-text>
+      </v-form>
+    </template>
+  </user-form-card>
 </template>
 
 <script>
 export default {
-  layout: 'beforeLogin'
+  layout: 'beforeLogin',
+  data () {
+    return {
+      isValid: false,
+      loading: false,
+      params: { user: { email: '', password: '' } }
+    }
+  },
+  methods: {
+    login () {
+      this.loading = true
+      setTimeout(() => (this.loading = false), 1500)
+    }
+  }
 }
 </script>
